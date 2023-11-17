b2ae0db8efa0a4b35ac47c9ae0b380ad2d63c3c6
= <majdeddinebentahar@gmail.com>
2023-11-17 09:16:43 +0100

    updated the project structure

---
+++ src/routes/portfolio/[project]/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Single project page</h1>
+<!-- -- add a gif of the project
+    -- add a more long description of the project
+    -- add the live demo link to the project in the description
+    -- add the source code link
+    -- add the technologies used in the project with icons
+    -- add the features of the project
+    
+ -->
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,61 @@
+<h1>Resume Page</h1>
+<!-- <h1>Coming Soon!!</h1> -->
+<!-- CV download button Use a clear and visible button to encourage clicks.-->
+<!-- add skills,latest projects(3 of them, and a link to portfolio page), experience, education -->
+
+<!-- Keep the content of your resume and CV straightforward and focused. -->
+
+<!-- 
+    
+    Objective or Summary:
+        Your Full Name
+        Brief tagline or your career goals or a summary of your professional experience.
+        
+    About Me:
+            A concise paragraph introducing yourself, your passion, and career objectives.
+            Keep it personal yet professional.
+    
+    Downloadable CV:
+        Include a download button/link for your detailed CV.
+            Put the button after the "About Me" or introduction section.
+            This is a logical placement because visitors may want to learn a bit about you before deciding to download your CV.
+            Use a clear and visible button to encourage clicks.
+            Make sure the button is linked to a PDF version of your CV.
+
+
+    Contact Information:
+        Phone Number
+        Email Address
+        LinkedIn Profile Link
+        Twitter Profile Link
+
+    Skills:
+        Highlight key technical and soft skills using a bullet-point format.
+        For example:
+            Web Development
+            Data Analysis
+            Project Management
+
+    Work Experience:
+        Showcase relevant work experience with short descriptions.
+        Focus on achievements and responsibilities.
+        Include job titles, company names, and dates.
+
+    Education:
+        List your academic achievements in reverse chronological order.
+        Mention degrees, institutions, and graduation dates.
+
+    Projects:
+        Include a section titled "Projects Portfolio."
+        Provide a brief introduction encouraging viewers to explore your full project portfolio on a separate page, example => Explore a collection of my completed projects to get a deeper understanding of my skills and work. Each project is detailed with links to live demos and source code. Visit [link to Projects Page] for a comprehensive view.
+
+    Certifications:
+        Mention any relevant certifications in a straightforward manner.
+
+    Languages:
+        List languages you are proficient in.
+
+    Contact/Connect:
+        Encourage visitors to get in touch or connect with you.
+        Include a call-to-action, such as "Feel free to reach out for collaboration opportunities!"
+-->
